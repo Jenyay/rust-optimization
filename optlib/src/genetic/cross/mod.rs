dc8a26dc069773b5489c323d5bf3045eca1f08d0
Jenyay <jenyay.ilin@gmail.com>
2019-02-14 10:31:13 +0300

    Add cross functions for primitive types

--- optlib/src/genetic/cross/mod.rs
+++ optlib/src/genetic/cross/mod.rs
@@ -66,7 +66,7 @@ impl FloatCrossBitwise {
 impl<G: Float> FloatCross<G> for FloatCrossBitwise {
     fn cross(&mut self, gene: &Vec<G>) -> Vec<G> {
         assert_eq!(gene.len(), 2);
-        let size = mem::size_of_val(&gene[0]) * 8;
+        let size = mem::size_of::<G>() * 8;
         let between = Uniform::new(1, size);
 
         let pos = between.sample(&mut self.random);
@@ -76,21 +76,73 @@ impl<G: Float> FloatCross<G> for FloatCrossBitwise {
                 let parent_1 = gene[0].to_f32().unwrap().to_bits();
                 let parent_2 = gene[1].to_f32().unwrap().to_bits();
 
-                let mask_parent_1 = (!(0 as u32)) << pos;
-                let mask_parent_2 = (!(0 as u32)) >> (size - pos);
-                let child_bits = (parent_1 & mask_parent_1) | (parent_2 & mask_parent_2);
+                let child_bits = cross_u32(parent_1, parent_2, pos);
                 vec![G::from(f32::from_bits(child_bits)).unwrap()]
             }
             64 => {
                 let parent_1 = gene[0].to_f64().unwrap().to_bits();
                 let parent_2 = gene[1].to_f64().unwrap().to_bits();
 
-                let mask_parent_1 = (!(0 as u64)) << pos;
-                let mask_parent_2 = (!(0 as u64)) >> (size - pos);
-                let child_bits = (parent_1 & mask_parent_1) | (parent_2 & mask_parent_2);
+                let child_bits = cross_u64(parent_1, parent_2, pos);
                 vec![G::from(f64::from_bits(child_bits)).unwrap()]
             }
             _ => panic!("Unknown gene type in cross"),
         }
     }
+}
+
+
+// pub trait CrossPrimitive {
+//     fn cross<T>(parent_1: T, parent_2: T, pos: usize) -> T;
+// }
+//
+// impl CrossPrimitive for u64 {
+//     fn cross<u64>(parent_1: u64, parent_2: u64, pos: usize) -> u64 {
+//         let size = mem::size_of_val(&parent_1) * 8;
+//         let mask_parent_1 = !0u64 << pos;
+//         let mask_parent_2 = !0u64 >> (size - pos);
+//         (parent_1 & mask_parent_1) | (parent_2 & mask_parent_2)
+//     }
+// }
+
+pub fn cross_u64(parent_1: u64, parent_2: u64, pos: usize) -> u64 {
+    let size = mem::size_of_val(&parent_1) * 8;
+    let mask_parent_1 = !0u64 << pos;
+    let mask_parent_2 = !0u64 >> (size - pos);
+    (parent_1 & mask_parent_1) | (parent_2 & mask_parent_2)
+}
+
+pub fn cross_u32(parent_1: u32, parent_2: u32, pos: usize) -> u32 {
+    let size = mem::size_of_val(&parent_1) * 8;
+    let mask_parent_1 = !0u32 << pos;
+    let mask_parent_2 = !0u32 >> (size - pos);
+    (parent_1 & mask_parent_1) | (parent_2 & mask_parent_2)
+}
+
+pub fn cross_i16(parent_1: i16, parent_2: i16, pos: usize) -> i16 {
+    let size = mem::size_of_val(&parent_1) * 8;
+    let mask_parent_1 = !0i16 << pos;
+    let mask_parent_2 = !0i16 >> (size - pos);
+    (parent_1 & mask_parent_1) | (parent_2 & mask_parent_2)
+}
+
+pub fn cross_u16(parent_1: u16, parent_2: u16, pos: usize) -> u16 {
+    let size = mem::size_of_val(&parent_1) * 8;
+    let mask_parent_1 = !0u16 << pos;
+    let mask_parent_2 = !0u16 >> (size - pos);
+    (parent_1 & mask_parent_1) | (parent_2 & mask_parent_2)
+}
+
+pub fn cross_i8(parent_1: i8, parent_2: i8, pos: usize) -> i8 {
+    let size = mem::size_of_val(&parent_1) * 8;
+    let mask_parent_1 = !0i8 << pos;
+    let mask_parent_2 = !0i8 >> (size - pos);
+    (parent_1 & mask_parent_1) | (parent_2 & mask_parent_2)
+}
+
+pub fn cross_u8(parent_1: u8, parent_2: u8, pos: usize) -> u8 {
+    let size = mem::size_of_val(&parent_1) * 8;
+    let mask_parent_1 = !0u8 << pos;
+    let mask_parent_2 = !0u8 >> (size - pos);
+    (parent_1 & mask_parent_1) | (parent_2 & mask_parent_2)
 }
