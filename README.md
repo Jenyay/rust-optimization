dc18fc7aaef1a642239e9d7a3876aa298de1cc54
Jenyay <jenyay.ilin@gmail.com>
2019-02-26 09:47:51 +0300

    Add more comments and example optimization on github main page

--- README.md
+++ README.md
@@ -1,2 +1,132 @@
-# rust-optimization
+# optlib
 Optimization algorithms realized in Rust
+
+In given time optlib realized genetic algorithm only.
+
+## Example of optimization
+
+```rust
+//! Example of optimizing the Schwefel function.
+//!
+//! y = f(x), where x = (x0, x1, ..., xi,... xn).
+//! Global minimum is x' = (420.9687, 420.9687, ...) for any xi lying in [-500.0; 500.0].
+//! f(x') = 0
+//!
+//! # Terms
+//! * 'Goal function' - the function for optimization. y = f(x).
+//! * `Gene` - a single value of xi.
+//! * 'Chromosome' - a point in the search space. x = (x0, x1, x2, ..., xn).
+//! * 'Individual' - union of x and value of goal function.
+//! * 'Population' - set of the individuals.
+//! * 'Generation' - a number of iteration of genetic algorithm.
+use optlib::genetic;
+use optlib::genetic::creation;
+use optlib::genetic::cross;
+use optlib::genetic::goal;
+use optlib::genetic::logging;
+use optlib::genetic::mutation;
+use optlib::genetic::pairing;
+use optlib::genetic::pre_birth;
+use optlib::genetic::selection;
+use optlib::genetic::stopchecker;
+use optlib::testfunctions;
+use optlib::Optimizer;
+
+/// Gene type
+type Gene = f32;
+
+/// Chromosomes type
+type Chromosomes = Vec<Gene>;
+
+fn main() {
+    // General parameters
+
+    // Search space. Any xi lies in [-500.0; 500.0]
+    let minval: Gene = -500.0;
+    let maxval: Gene = 500.0;
+
+    // Count individuals in initial population
+    let population_size = 500;
+
+    // Count of xi in the chromosomes
+    let chromo_count = 15;
+
+    let intervals = vec![(minval, maxval); chromo_count];
+
+    // Make a trait object for goal function (Schwefel function)
+    let goal = goal::GoalFromFunction::new(testfunctions::schwefel);
+
+    // Make the creator to create initial population.
+    // RandomCreator will fill initial population with individuals with random chromosomes in a
+    // given interval,
+    let creator = creation::vec_float::RandomCreator::new(population_size, intervals.clone());
+
+    // Make a trait object for the pairing.
+    // Pairing is algorithm of selection individuals for crossbreeding.
+
+    // Select random individuals from the population.
+    // let pairing = pairing::RandomPairing::new();
+
+    // Tournament method.
+    let partners_count = 2;
+    let families_count = population_size / 2;
+    let rounds_count = 5;
+    let pairing = pairing::Tournament::new(partners_count, families_count, rounds_count);
+
+    // Crossbreeding algorithm.
+    // Make a Cross trait object. The bitwise crossing for float genes.
+    let single_cross = cross::FloatCrossExp::new();
+    let cross = cross::VecCrossAllGenes::new(Box::new(single_cross));
+
+    // Make a Mutation trait object.
+    // Use bitwise mutation (change random bits with given probability).
+    let mutation_probability = 15.0;
+    let mutation_gene_count = 3;
+    let single_mutation = mutation::BitwiseMutation::new(mutation_gene_count);
+    let mutation = mutation::VecMutation::new(mutation_probability, Box::new(single_mutation));
+
+    // Pre birth. Throw away new chlld chromosomes if their genes do not lies if given intervals.
+    let pre_births: Vec<Box<genetic::PreBirth<Chromosomes>>> = vec![Box::new(
+        pre_birth::vec_float::CheckChromoInterval::new(intervals.clone()),
+    )];
+
+    // Stop checker. Stop criterion for genetic algorithm.
+    // Stop algorithm if the value of goal function will become less of 1e-4 or
+    // after 3000 generations (iterations).
+    let stop_checker = stopchecker::CompositeAny::new(vec![
+        Box::new(stopchecker::Threshold::new(1e-4)),
+        Box::new(stopchecker::MaxIterations::new(3000)),
+    ]);
+
+    // Make a trait object for selection. Selection is killing the worst individuals.
+    // Kill all individuals if the value of goal function is NaN or Inf.
+    // Kill the worst individuals to population size remained unchanged.
+    let selections: Vec<Box<dyn genetic::Selection<Chromosomes>>> = vec![
+        Box::new(selection::KillFitnessNaN::new()),
+        Box::new(selection::LimitPopulation::new(population_size)),
+    ];
+
+    // Make a loggers trait objects
+    let loggers: Vec<Box<genetic::Logger<Chromosomes>>> = vec![
+        Box::new(logging::StdoutResultOnlyLogger::new(15)),
+        Box::new(logging::TimeStdoutLogger::new()),
+    ];
+
+    // Construct main optimizer struct
+    let mut optimizer = genetic::GeneticOptimizer::new(
+        Box::new(goal),
+        Box::new(stop_checker),
+        Box::new(creator),
+        Box::new(pairing),
+        Box::new(cross),
+        Box::new(mutation),
+        selections,
+        pre_births,
+        loggers,
+    );
+
+    // Run genetic algorithm
+    optimizer.find_min();
+}
+
+```
--- examples/genetic-schwefel/src/main.rs
+++ examples/genetic-schwefel/src/main.rs
@@ -1,3 +1,16 @@
+//! Example of optimizing the Schwefel function.
+//!
+//! y = f(x), where x = (x0, x1, ..., xi,... xn).
+//! Global minimum is x' = (420.9687, 420.9687, ...) for any xi lying in [-500.0; 500.0].
+//! f(x') = 0
+//!
+//! # Terms
+//! * 'Goal function' - the function for optimization. y = f(x).
+//! * `Gene` - a single value of xi.
+//! * 'Chromosome' - a point in the search space. x = (x0, x1, x2, ..., xn).
+//! * 'Individual' - union of x and value of goal function.
+//! * 'Population' - set of the individuals.
+//! * 'Generation' - a number of iteration of genetic algorithm.
 use optlib::genetic;
 use optlib::genetic::creation;
 use optlib::genetic::cross;
@@ -11,54 +24,72 @@ use optlib::genetic::stopchecker;
 use optlib::testfunctions;
 use optlib::Optimizer;
 
+/// Gene type
 type Gene = f32;
+
+/// Chromosomes type
 type Chromosomes = Vec<Gene>;
 
 fn main() {
     // General parameters
+
+    // Search space. Any xi lies in [-500.0; 500.0]
     let minval: Gene = -500.0;
     let maxval: Gene = 500.0;
+
+    // Count individuals in initial population
     let population_size = 500;
+
+    // Count of xi in the chromosomes
     let chromo_count = 15;
+
     let intervals = vec![(minval, maxval); chromo_count];
 
-    // Goal function
+    // Make a trait object for goal function (Schwefel function)
     let goal = goal::GoalFromFunction::new(testfunctions::schwefel);
 
-    // Creator
+    // Make the creator to create initial population.
+    // RandomCreator will fill initial population with individuals with random chromosomes in a
+    // given interval,
     let creator = creation::vec_float::RandomCreator::new(population_size, intervals.clone());
 
-    // Pairing
+    // Make a trait object for the pairing.
+    // Pairing is algorithm of selection individuals for crossbreeding.
+
+    // Select random individuals from the population.
     // let pairing = pairing::RandomPairing::new();
+
+    // Tournament method.
     let partners_count = 2;
     let families_count = population_size / 2;
     let rounds_count = 5;
     let pairing = pairing::Tournament::new(partners_count, families_count, rounds_count);
 
-    // Cross
-    // let single_cross = cross::CrossMean::new();
+    // Crossbreeding algorithm.
+    // Make a Cross trait object. The bitwise crossing for float genes.
     let single_cross = cross::FloatCrossExp::new();
-    // let single_cross = cross::CrossBitwise::new();
     let cross = cross::VecCrossAllGenes::new(Box::new(single_cross));
 
-    // Mutation
+    // Make a Mutation trait object.
+    // Use bitwise mutation (change random bits with given probability).
     let mutation_probability = 15.0;
     let mutation_gene_count = 3;
     let single_mutation = mutation::BitwiseMutation::new(mutation_gene_count);
-    // let single_cross = cross::CrossMean::new();
-    // let single_cross = cross::FloatCrossGeometricMean::new();
     let mutation = mutation::VecMutation::new(mutation_probability, Box::new(single_mutation));
 
-    // Pre birth
+    // Pre birth. Throw away new chlld chromosomes if their genes do not lies if given intervals.
     let pre_births: Vec<Box<genetic::PreBirth<Chromosomes>>> = vec![Box::new(
         pre_birth::vec_float::CheckChromoInterval::new(intervals.clone()),
     )];
 
-    // Stop checker
+    // Stop checker. Stop criterion for genetic algorithm.
     // let change_max_iterations = 200;
     // let change_delta = 1e-7;
     // let stop_checker = stopchecker::GoalNotChange::new(change_max_iterations, change_delta);
     // let stop_checker = stopchecker::MaxIterations::new(500);
+
+    // Stop algorithm if the value of goal function will become less of 1e-4 or
+    // after 3000 generations (iterations).
     let stop_checker = stopchecker::CompositeAny::new(vec![
         Box::new(stopchecker::Threshold::new(1e-4)),
         // Box::new(stopchecker::GoalNotChange::new(
@@ -68,19 +99,22 @@ fn main() {
         Box::new(stopchecker::MaxIterations::new(3000)),
     ]);
 
-    // Selection
+    // Make a trait object for selection. Selection is killing the worst individuals.
+    // Kill all individuals if the value of goal function is NaN or Inf.
+    // Kill the worst individuals to population size remained unchanged.
     let selections: Vec<Box<dyn genetic::Selection<Chromosomes>>> = vec![
         Box::new(selection::KillFitnessNaN::new()),
         Box::new(selection::LimitPopulation::new(population_size)),
     ];
 
-    // Logger
+    // Make a loggers trait objects
     let loggers: Vec<Box<genetic::Logger<Chromosomes>>> = vec![
         // Box::new(logging::VerboseStdoutLogger::new(15)),
         Box::new(logging::StdoutResultOnlyLogger::new(15)),
         Box::new(logging::TimeStdoutLogger::new()),
     ];
 
+    // Construct main optimizer struct
     let mut optimizer = genetic::GeneticOptimizer::new(
         Box::new(goal),
         Box::new(stop_checker),
@@ -93,5 +127,6 @@ fn main() {
         loggers,
     );
 
+    // Run genetic algorithm
     optimizer.find_min();
 }
